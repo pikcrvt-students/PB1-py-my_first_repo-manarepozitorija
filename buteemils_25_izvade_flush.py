41f19193f2e97a59485d77baf4c72cb613c47b20
25DP3EBute <A251468EB@rvt.lv>
2026-03-13 12:56:07 +0200

    funkcija print ar parametru flush,vertibu pec noklusejuma(defoult) ir False.

---
+++ buteemils_25_izvade_flush.py
@@ -0,0 +1,14 @@
+"""
+    Print function with flush parameter
+    Created by : Emils Bute
+
+    URL : https://www.includehelp.com/python/flush-parameter-in-python-with-print-function.aspx
+"""
+# output not flushed here
+print("Es macos programmet.  ", end='')
+print("Lietoju funkciju print.  ", end='')
+print("Izmantoju paramentu flush.  ", end='')
+
+print("Bye!!!")
+input("Press <ENTER> to exit.")
+
