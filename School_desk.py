c369df2854059f886fec864cbc66ab5fd5c82a52
25DP3EBute <A251468EB@rvt.lv>
2026-04-17 13:14:03 +0300

    Skolas galdu apreiķins

---
+++ School_desk.py
@@ -0,0 +1,11 @@
+Klase_a = int(input("Ievadi 1 skaitlis "))
+Klase_b = int(input("Ievadi 2 skaitlis "))
+Klase_c = int(input("Ievadi 3 skaitlis "))
+
+galdu_skaits_a_klase = Klase_a // 2 + Klase_a % 2
+galdu_skaits_b_klase = Klase_b // 2 + Klase_b % 2
+galdu_skaits_c_klase = Klase_c // 2 + Klase_c % 2
+
+print("Soļu skaits 3 klasēs ir",galdu_skaits_a_klase + galdu_skaits_b_klase + galdu_skaits_c_klase)
+
+
