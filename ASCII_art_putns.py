d31df929b111882091731e0ae6ffd52cd89cb01e
25DP3EBute <A251468EB@rvt.lv>
2026-04-20 12:15:04 +0300

    programma ar skanu

--- ASCII_art_putns.py
+++ ASCII_art_putns.py
@@ -2,4 +2,21 @@ import os
 from time import sleep
 
 for i in range(0, 5)
-os.system
+os.system("cls" if os.name ==  "nt" else "clear")
+print("""
+
+  _____<>
+>-\//_/
+  //
+
+     """) 
+sleep(.5)
+
+os.system("cls" if os.name ==  "nt" else "clear")
+print("""
+
+  \\
+  _\\\__<>
+>-\___/
+      """)
+sleep(.5)
---
+++ programma_ar_skanu.py
@@ -0,0 +1,6 @@
+import winsound
+
+frequancy = 2000
+duration = 1500
+
+winsound.Beep(frequancy, duration)
