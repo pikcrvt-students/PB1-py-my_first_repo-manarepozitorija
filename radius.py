7c8760e9bd2c75a209f8d9ca8520b8b1cfa761bc
25DP3EBute <A251468EB@rvt.lv>
2026-05-11 13:31:31 +0300

    uzlabojums ar pi

--- radius.py
+++ radius.py
@@ -1,7 +1,9 @@
+import math
 radiuss = int(input("ievadi radiusu "))
 
 while radiuss <= 0:
     radiuss = int(input("ievadi radiusu "))
 
-L = 2*3.1415* radiuss
+L = 2 * math.pi * radiuss
+
 print(L) 
