4c5ebbddce7b65bdde591de1ecbe1c80a1b88acd
25DP3EBute <A251468EB@rvt.lv>
2026-05-11 13:23:21 +0300

    uzlabojumi

--- radius.py
+++ radius.py
@@ -1,6 +1,7 @@
-radiuss = int(input("ievadi radiusu"))
+radiuss = int(input("ievadi radiusu "))
 
 while radiuss <= 0:
-    radiuss = int(input("ievadi radiusu"))
-L = 2*3.1415 * radiuss
-print(L)
+    radiuss = int(input("ievadi radiusu "))
+
+L = 2*3.1415* radiuss
+print(L) 
